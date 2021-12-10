999044823b579ac338f150a296e6ae98d4eb9e40
Hp <Hp@DESKTOP-8SJFKNP>
2021-12-10 13:19:24 +0530

    uygbjhvouyh uyj

---
+++ src/test/java/github11/Demo.java
@@ -0,0 +1,12 @@
+package github11;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void demoClass() {
+		System.out.println("iouljiu;ij ioih o");
+		System.out.println("iu;biuqpiub ;QWDIUWQBPDI KJ");
+	}
+
+}
